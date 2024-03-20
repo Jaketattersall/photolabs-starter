ed0bca6544b8c99bfa3337e57e0b7793e4f498e5
jaketattersall <jaketattersall90@gmail.com>
2024-03-20 15:36:24 -0700

    Add the Favourite Icon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,19 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+    const [likeButton, setlikeButton] = useState(false);
+    const clickLikeButton = () => {
+      setlikeButton((prev) => !prev);
+    };
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={clickLikeButton}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+       
+        {likeButton ? (
+          <i className="fa-solid fa-heart" style={{ color: "red" }} />
+        ) : (
+          <i className="fa-regular fa-heart" style={{ color: "red" }} />
+        )}
       </div>
     </div>
   );
